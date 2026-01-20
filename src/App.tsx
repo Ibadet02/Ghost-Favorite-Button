4d39afb014a1807437cc4643f3c3b5a043970100
Ibadet02 <ibadetismayilov20@gmail.com>
2026-01-20 23:59:31 +0100

    create a simple like counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
 
 
 function App() {
-  return <div></div>
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count + 1);
+  }
+  return <div>
+    <button onClick={handleCount}>like {count} times</button>
+  </div>
 }
 
 export default App
